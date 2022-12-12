a2784f86df4e6f610131b6fbf2d843244fd006c9
mr_darcy <mr_darcy_1@mail.ru>
2022-12-12 16:35:23 +0500

    Add files via upload

---
+++ 04_homework/script.js
@@ -0,0 +1,11 @@
+// task 1 --------------------
+/*
+key = 7859d9d42a8834141d529577207c9596
+*/
+function f1(){
+    let a = 4;
+    if (a === 4){
+        console.log("Оно равно 4");
+    }
+}
+document.querySelector('.task-1').onclick = f1;
